82489f3b0f43ab353689a5a4ee4077d5de417461
AbDuL RaZaK Mo <r17.abdul@gmail.com>
2020-04-11 09:33:19 +0000

    deploment 3

--- controllers/controller.js
+++ controllers/controller.js
@@ -17,12 +17,12 @@ exports.getEstimatesXml = (req, res) => {
   res.set('Content-Type', 'application/xml');
   const data = req.body;
   let estimates;
-  estimates = estimator(data);
-  res.status(200).send(jsonxml(estimates));
+  estimates = jsonxml(estimator(data));
+  res.status(200).send(estimates);
 };
 
 exports.getLogData = (req, res) => {
   res.set("Content-Type", "text/html");
   const logData = fs.readFileSync(filePath, "utf8");
-  res.status(200).send( logData);
+  res.status(200).send(logData);
 };
